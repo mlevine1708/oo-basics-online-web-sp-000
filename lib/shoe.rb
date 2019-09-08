ffaea42834e0485b782d47fc3e44be755293e4dd
Micah Levine <micah.levine@outlook.com>
2019-09-08 23:58:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,12 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :size, :material
+  def initialize(brand)
+    @brand = brand
+  end
 
-  
+  def brand
+    @brand
+  end
+
+
 end
